992e4e9d0ba3c2448202a0dae4cf07a49e73908c
Mike Cluck <mcluck90@gmail.com>
2018-12-02 11:46:39 -0700

    Finished day 1

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
   input
@@ -12,27 +14,63 @@ pub fn part1(input: &[i32]) -> i32 {
   input.iter().sum()
 }
 
+#[aoc(day1, part2)]
+pub fn part2(input: &[i32]) -> i32 {
+  let mut seen = HashSet::new();
+  let mut frequency = 0;
+  seen.insert(0);
+  loop {
+    for val in input {
+      frequency += val;
+      if seen.contains(&frequency) {
+        return frequency;
+      }
+      seen.insert(frequency);
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
-  fn sample1() {
+  fn part1_sample1() {
     assert_eq!(part1(&vec![1, -2, 3, 1]), 3);
   }
 
   #[test]
-  fn sample2() {
+  fn part1_sample2() {
     assert_eq!(part1(&vec![1, 1, 1]), 3);
   }
 
   #[test]
-  fn sample3() {
+  fn part1_sample3() {
     assert_eq!(part1(&vec![1, 1, -2]), 0);
   }
 
   #[test]
-  fn sample4() {
+  fn part1_sample4() {
     assert_eq!(part1(&vec![-1, -2, -3]), -6);
+  }
+
+  #[test]
+  fn part2_sample1() {
+    assert_eq!(part2(&vec![1, -1]), 0);
+  }
+
+  #[test]
+  fn part2_sample2() {
+    assert_eq!(part2(&vec![3, 3, 4, -2, -4]), 10);
+  }
+
+  #[test]
+  fn part2_sample3() {
+    assert_eq!(part2(&vec![-6, 3, 8, 5, -6]), 5);
+  }
+
+  #[test]
+  fn part2_sample4() {
+    assert_eq!(part2(&vec![7, 7, -2, -7, -4]), 14);
   }
 }
