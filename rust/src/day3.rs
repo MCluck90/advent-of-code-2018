b5a83a8bb0726dc1199e941426643082ad36278b
Mike Cluck <mcluck90@gmail.com>
2018-12-05 08:07:23 -0700

    Added overlap algorithm

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -1,10 +1,33 @@
 #[derive(Debug, PartialEq)]
 pub struct Rect {
-	id: u32,
-	x: u32,
-	y: u32,
-	w: u32,
-	h: u32,
+	pub id: u32,
+	pub x: u32,
+	pub y: u32,
+	pub w: u32,
+	pub h: u32,
+}
+
+impl Rect {
+	pub fn overlap(&self, other: &Rect) -> Option<Rect> {
+		use std::cmp::{max, min};
+		if self.x < other.x + other.w
+			&& self.x + self.w > other.x
+			&& self.y < other.y + other.w
+			&& self.y + self.h > other.y
+		{
+			let self_right = self.x + self.w;
+			let other_right = other.x + other.w;
+			let self_bottom = self.y + self.h;
+			let other_bottom = other.y + other.h;
+			let x = max(self.x, other.x);
+			let y = max(self.y, other.y);
+			let w = min(self_right, other_right) - x;
+			let h = min(self_bottom, other_bottom) - y;
+			Some(Rect { id: 0, x, y, w, h })
+		} else {
+			None
+		}
+	}
 }
 
 #[aoc_generator(day3)]
@@ -15,7 +38,7 @@ pub fn input_generator(input: &str) -> Vec<Rect> {
 		let mut pieces = line.split(' ');
 		// Pull out the unparsed sections
 		let id = pieces.next().unwrap();
-		let _ = pieces.next().unwrap();
+		let _ = pieces.next().unwrap(); // Ignore the @
 		let mut coords = pieces.next().unwrap().split(',');
 		let mut size = pieces.next().unwrap().split('x');
 
@@ -57,6 +80,95 @@ mod tests {
 			h: 16,
 		};
 		assert_eq!(output[0], expected);
+	}
+
+	#[test]
+	fn complete_overlap() {
+		let first = Rect {
+			id: 0,
+			x: 0,
+			y: 0,
+			w: 10,
+			h: 10,
+		};
+		let second = Rect {
+			id: 0,
+			x: 0,
+			y: 0,
+			w: 10,
+			h: 10,
+		};
+		let overlap = first.overlap(&second).unwrap();
+		assert_eq!(overlap.x, 0);
+		assert_eq!(overlap.y, 0);
+		assert_eq!(overlap.w, 10);
+		assert_eq!(overlap.h, 10);
+	}
+
+	#[test]
+	fn partial_overlap() {
+		let first = Rect {
+			id: 0,
+			x: 0,
+			y: 0,
+			w: 10,
+			h: 10,
+		};
+		let second = Rect {
+			id: 0,
+			x: 5,
+			y: 5,
+			w: 10,
+			h: 10,
+		};
+		let overlap = first.overlap(&second).unwrap();
+		assert_eq!(overlap.x, 5);
+		assert_eq!(overlap.y, 5);
+		assert_eq!(overlap.w, 5);
+		assert_eq!(overlap.h, 5);
+	}
+
+	#[test]
+	fn complete_internal_overlap() {
+		let first = Rect {
+			id: 0,
+			x: 0,
+			y: 0,
+			w: 10,
+			h: 10,
+		};
+		let second = Rect {
+			id: 0,
+			x: 2,
+			y: 2,
+			w: 3,
+			h: 3,
+		};
+		let overlap = first.overlap(&second).unwrap();
+		assert_eq!(overlap.x, 2);
+		assert_eq!(overlap.y, 2);
+		assert_eq!(overlap.w, 3);
+		assert_eq!(overlap.h, 3);
+	}
+
+	#[test]
+	fn no_overlap() {
+		let first = Rect {
+			id: 0,
+			x: 0,
+			y: 0,
+			w: 10,
+			h: 10,
+		};
+		let second = Rect {
+			id: 0,
+			x: 20,
+			y: 20,
+			w: 10,
+			h: 10,
+		};
+		let overlap = first.overlap(&second);
+		assert_eq!(overlap, None);
 	}
 
 	#[test]
